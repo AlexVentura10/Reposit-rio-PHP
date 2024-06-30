1d985e2b39c830152efa02b7a0bd5fcde90ad5d2
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-06-30 13:05:44 -0300

    Ex1
    Commit 1

---
+++ Ex1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+ 
+ <?php 
+  
+  echo "<h1>Olá mundo!\u{1F30E} <h1>"
+  // status teste
+  
+  ?>
+    </h1>
+    <p>Vamos tentar se livrar da maldição</p>
+</body>
+</html>
