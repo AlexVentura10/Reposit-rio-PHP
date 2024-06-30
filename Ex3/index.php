0170924f75c9de27d9edbebefdb018b4295cbb2d
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-06-30 13:09:49 -0300

    Ex3
    Incorporando PHP em HTML

---
+++ Ex3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head> <!-- Cabeça -->
+    <meta charset="UTF-8"> <!-- Caracteres suportados-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- 100%-->  
+    <title>Incorporando PHP em HTML</title>
+</head>
+
+<body> <!-- Corpo -->
+
+     <h1>Incorporando PHP em HTML</h1>
+
+<?php     // Super tag PHP                                     
+
+  echo "Código PHP Incorporado em HTML \u{1F4BB}"
+   
+?>       
+
+</body>
+
+</html>
