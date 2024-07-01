d83316f3de22222acc6f2e3e0f4a18a316a5e96b
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-07-01 16:44:36 -0300

    Update index.php

--- Ex3/index.php
+++ Ex3/index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 
-<html lang="pt-br">
+<html lang="pt-br"> 
 
 <head> <!-- Cabeça -->
     <meta charset="UTF-8"> <!-- Caracteres suportados-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--100%t-->  
-    <title>Incorporando PHP em HTML</title>
+    <title>Incorporando PHP em HTML </title> <!-- Nome da guia do navegador -->
 </head>
 
 <body> <!-- Corpo -->
 
-     <h1>Incorporando PHP em HTML</h1>
+     <h1>Incorporando PHP em HTML</h1> <!-- Título --> 
 
 <?php     // Super tag PHP                                     
 
