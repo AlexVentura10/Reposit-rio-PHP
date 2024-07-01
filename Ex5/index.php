cba81e8ef34487aacfd1ef70add0022fe4637600
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-07-01 14:24:43 -0300

    Ex5
    Uso variáveis, constantes, regras e recomendações.

---
+++ Ex5/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis e Constantes</title>
+</head>
+<body>
+    <h1> Declarando Variáveis e Constantes</h1>
+    <?php 
+    
+    $nome = "Alex"; // Variável
+    const Pais = "Brasil"; // Constante
+    echo "Olá, $nome, tudo bem? Você mora no " .Pais;
+    
+    // ------------------------------------------------
+
+    // Utilização de "camelCase" para métodos e atributos
+
+    $nomeCompleto = "Camel Case";
+    $telefone_particular = "Snake CASE";
+    $endereçodocliente = "Não é errado, mas não recomendado e confuso"
+    
+    ?>
+</body>
+</html>
