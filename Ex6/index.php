a9d33cb4a77aace8768b3ad6b986c8da3422bfd4
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-07-02 13:41:43 -0300

    Update index.php

--- Ex6/index.php
+++ Ex6/index.php
@@ -8,8 +8,20 @@
 <body>
     <h1>Tipos Primitivos</h1>
     <?php 
+     /*
      $tipoPrimitivo = 300;
      var_dump($tipoPrimitivo); // Essa função mostrará o tipo de primitivo da variável.
+     */ 
+
+     $num1 = 3e2;
+     echo " 3e2 = $num1 <br> Tipo primitivo: "; // 3e2 = 3x10(2)
+     var_dump($num1); // Uma vez potência, sempre obteremos float 
+     
+     echo "<br>"; 
+
+     $num2 = "427";
+     var_dump($num2);
+
     ?>
 
 </body>
