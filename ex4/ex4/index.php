7b401923a8e3d60caeff9a300367c8ccb074f0d1
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-07-02 15:06:10 -0300

    Concatenação e configuração de data e hora

--- ex4/ex4/index.php
+++ ex4/ex4/index.php
@@ -8,9 +8,12 @@
 <body>
     <H1>Concatenação e configuração de data e hora</H1>
     <?php 
+    
     date_default_timezone_set("America/Sao_Paulo"); // <--Configure a área padrão da data
-     echo "Hoje é dia: ". date("d/M/Y"); // Função
-     echo " Hora atual: ". date("G:i:s: T") // Função
+    
+    echo "Hoje é dia: ". date("d/M/Y"); // Função
+     
+    echo " Hora atual: ". date("G:i:s: T") // Função
 
      // Sem o scrit da linha 11, a configuração timezone, por padrão, fica a do servidor (CET) e não a timezone cliente (UTC).
 
