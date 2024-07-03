2c448eb3f104ae51ec9bf3867ac65acac8255b94
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-07-03 18:16:22 -0300

    Tipos Primitivos

--- ex3/index.php
+++ ex3/index.php
@@ -8,19 +8,47 @@
 <body>
     <h1>Tipos Primitivos</h1>
     <?php 
-     /*
+    
      $tipoPrimitivo = 300;
+     
      var_dump($tipoPrimitivo); // Essa função mostrará o tipo de primitivo da variável.
-     */ 
+     
+     // 
 
      $num1 = 3e2;
+
      echo " 3e2 = $num1 <br> Tipo primitivo: "; // 3e2 = 3x10(2)
+
      var_dump($num1); // Uma vez potência, sempre obteremos float 
      
+     // 
+
      echo "<br>"; 
 
      $num2 = "427";
+
      var_dump($num2);
+
+     echo "<br>";
+
+     // Vetor 
+
+     $vet = [6,"Alex",7.5,9,3];
+     var_dump($vet);
+    
+     echo "<br>";
+
+     // Class and Object (Tipo primitivo do tipo composto)
+  
+    class Pessoa
+    { 
+      public string $nome;
+    }
+     $usuario = new Pessoa;
+
+     $usuario = "Alex";
+
+     echo "O nome do usuário é: $usuario";
 
     ?>
 
