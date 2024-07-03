2d8e07060cf07e53a6103f66e8b3e46532a8df1f
Alexvt23 <158000770+AlexVentura10@users.noreply.github.com>
2024-07-03 18:52:53 -0300

    Update index.php

--- ex6/index.php
+++ ex6/index.php
@@ -6,10 +6,12 @@
     <title>Tipos de String</title>
 </head>
 <body>
+
     <h1>Tipos de String</h1>
     <?php 
     // Tipo Double quoted (""): Com o uso das aspas duplas existe a interpretação do conteúdo. EX:
 
+    echo "Tipo double quoted (aspas duplas) <br>";
     echo "Olá Mundo \u{1F30E}"
     
     // O "\u" trata-se de uma sequência de escape, utilizada a partir do PHO vs 7, para exibir caracteres especiais unicod. Ex: Emoji  Código do emoji world: \u{1F30E}
